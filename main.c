654a9eb31c988cd8e4f5ace1bf6c98d27516f1a6
ladejtech <ladej.tech@gmail.com>
2025-01-17 13:05:28 +0100

    Update main.c
    Change has me made as required

--- main.c
+++ main.c
@@ -1,9 +1,9 @@
 int main()
 {
   int i;
-  for (i = 0; i < 10; i++)
+  for (i = 0; i < 32; i++) // Changed 10 to 32
   {
-    printf("School\n");
+    printf("Hello School\n"); // Changed "School" to "Hello School"
   }
   return 0;
 }
